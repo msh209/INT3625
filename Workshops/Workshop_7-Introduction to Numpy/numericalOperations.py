d4b8dd0f7a6a360c57c42173ef7e696fc6ec2f1c
Michael Hughes <71533273+msh209@users.noreply.github.com>
2024-01-09 21:56:40 +0000

    Add files via upload

---
+++ Workshops/Workshop_7-Introduction to Numpy/numericalOperations.py
@@ -0,0 +1,17 @@
+'''A totally useless module for performing numerical operations using functions'''
+
+def addition(a,b):
+    '''A totally useless function for adding numbers'''
+    return a+b
+
+def subtraction(a,b):
+    '''A totally useless function for subtracting numbers'''
+    return a-b
+
+def multiplication(a,b):
+    '''A totally useless function for multiplying numbers'''
+    return a*b
+
+def division(a,b):
+    '''A totally useless function for dividing numbers'''
+    return a/b
